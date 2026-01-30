158978e5ee13934c47d0cc339f7b394178529e56
Roos Harteveld <harteveld.rr@gmail.com>
2026-01-30 21:59:42 +0100

    les 6

---
+++ Les 6/dictionary.py
@@ -0,0 +1,8 @@
+mijn_dictionary = {
+    "product": "softijs",
+    "aantal": 101,
+    "smaak": "vanille"
+}
+print(mijn_dictionary ["aantal"])
+#keys = mijn_dictionary.keys() als je alle sleutels aan 1 variabele wilt toekennen
+#values = mijn_dictionary.values() hetzelfde met de waarden
---
+++ Les 6/dictionary2.py
@@ -0,0 +1,11 @@
+mijn_dictionary = {
+    "product": "softijs",
+    "aantal": 101,
+    "smaak": "vanille"
+}
+keys=mijn_dictionary.keys()
+values=mijn_dictionary.values()
+print ("keys:",keys)
+print ("values:",values)
+#for item in mijn_dictionary: enter print(item) sleutels van de dictionary
+#for item in mijn_dictionary: enter print (mijn_dictionary[item]) waarden itereren
