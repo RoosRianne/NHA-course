abd0a93c56a88ba9fff26a76d8a15ba6c32944f6
Roos Harteveld <harteveld.rr@gmail.com>
2026-02-10 16:52:55 +0100

    opmerkingen toegevoegd

--- Les 5/HW3.py
+++
@@ -1,4 +0,0 @@
-prijs1 = 10
-prijs2 = 0.50
-totaal = prijs1 * prijs2
-print (totaal)
---
+++ Les 5/int_float.py
@@ -0,0 +1,11 @@
+prijs1 = 10
+prijs2 = 0.50
+totaal = prijs1 * prijs2
+print (totaal)
+# Prijs 1 is een integer getal (int).
+# Prijs 2 is een floating-point getal (float).
+# Python doet dit automatisch. 
+# Andere programma's moeten aangegeven worden. 
+# int prijs1 = 10
+# float prijs2 = 0.50
+# Wij gebruiken komma's maar Amerika gebruikt punten, voor een getal achter de komma. 
